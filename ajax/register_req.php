1bdb3a4d7fd948b3901383bfcb90beba265b2ebc
BhathiyaTK <bhathiyakariyawasam94@gmail.com>
2019-07-14 07:28:05 +0530

    updated file

--- ajax/register_req.php
+++ ajax/register_req.php
@@ -22,58 +22,62 @@ $std_appr = "No Action Taken";
 
 if (($full_name!=='')&&($name_on_badge!=='')&&($affiliation!=='')&&($country!=='')&&($email!=='')&&($partipant_type!=='')) {
 
-	if (($partipant_type == 'participant/co-author')||($partipant_type == 'author')) {
-		$studentship = "N/A";
-		
-		$sql = "INSERT INTO deligates(name,id_or_title,badge_name,affiliation,country,email,phone,rec_name,rec_address,part_type,bank_rec,std_ship,lunch,dinner,tour,bank_rec_appr,stdship_appr) VALUES('$full_name','$paper_id','$name_on_badge','$affiliation','$country','$email','$phone','$receipt_name','$receipt_address','$partipant_type','$payment_receipt','$studentship','$lunch','$dinner','$tour','$bank_appr','$std_appr')";
+	if ((preg_match('/^\+[0-9]{1,2}-[0-9]{9}$/', $phone))||(preg_match('/^[0-9]{3}-[0-9]{9}$/', $phone))||(preg_match('/^\+[0-9]{1,2}[0-9]{9}$/', $phone))||(preg_match('/^\+[0-9]{1,2}-[0-9]{2}-[0-9]{3}-[0-9]{4}$/', $phone))||(preg_match('/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/', $phone))||(preg_match('/^[0-9]{10}$/', $phone))||(preg_match('/^\+[0-9]{2,4}-[0-9]{7}$/', $phone))) {
+		if (($partipant_type == 'participant/co-author')||($partipant_type == 'author')) {
+			$studentship = "N/A";
+			
+			$sql = "INSERT INTO deligates(name,id_or_title,badge_name,affiliation,country,email,phone,rec_name,rec_address,part_type,bank_rec,std_ship,lunch,dinner,tour,bank_rec_appr,stdship_appr) VALUES('$full_name','$paper_id','$name_on_badge','$affiliation','$country','$email','$phone','$receipt_name','$receipt_address','$partipant_type','$payment_receipt','$studentship','$lunch','$dinner','$tour','$bank_appr','$std_appr')";
 
-		if ($conn->query($sql)) {
+			if ($conn->query($sql)) {
 
-			if ((!is_dir('../images/uploads/'.$name_on_badge))&&(!file_exists('../images/uploads/'.$name_on_badge))) {
-				mkdir('../images/uploads/'.$name_on_badge,0777);
+				if ((!is_dir('../images/uploads/'.$name_on_badge))&&(!file_exists('../images/uploads/'.$name_on_badge))) {
+					mkdir('../images/uploads/'.$name_on_badge,0777);
 
-				move_uploaded_file($_FILES['payment_receipt']['tmp_name'], '../images/uploads/'.$name_on_badge.'/'.$payment_receipt);
+					move_uploaded_file($_FILES['payment_receipt']['tmp_name'], '../images/uploads/'.$name_on_badge.'/'.$payment_receipt);
 
+					echo '<div class="alert alert-success alert-dismissible fade show" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><i class="fas fa-check-circle fa-lg"></i><b> Registered successfully.</b></div>';
+				}else{
+					echo '<div class="alert alert-danger alert-dismissible fade show" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><i class="fas fa-exclamation-triangle"></i><b> Please use different names for </b></div>';
+				}
+			}else{
+				echo '<div class="alert alert-danger alert-dismissible fade show" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><i class="fas fa-exclamation-triangle"></i><b> Database connection lost.</b></div>';
+			}
+		}elseif ($partipant_type == 'student') {
+			
+			$sql = "INSERT INTO deligates(name,id_or_title,badge_name,affiliation,country,email,phone,rec_name,rec_address,part_type,bank_rec,std_ship,lunch,dinner,tour,bank_rec_appr,stdship_appr) VALUES('$full_name','$paper_id','$name_on_badge','$affiliation','$country','$email','$phone','$receipt_name','$receipt_address','$partipant_type','$payment_receipt','$studentship','$lunch','$dinner','$tour','$bank_appr','$std_appr')";
+
+			if ($conn->query($sql)) {
+
+				if ((!is_dir('../images/uploads/'.$name_on_badge))&&(!file_exists('../images/uploads/'.$name_on_badge))) {
+					mkdir('../images/uploads/'.$name_on_badge,0777);
+
+					move_uploaded_file($_FILES['payment_receipt']['tmp_name'], '../images/uploads/'.$name_on_badge.'/'.$payment_receipt);
+					move_uploaded_file($_FILES['studentship']['tmp_name'], '../images/uploads/'.$name_on_badge.'/'.$studentship);
+
+					echo '<div class="alert alert-success alert-dismissible fade show" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><i class="fas fa-check-circle fa-lg"></i><b> Registered successfully.</b></div>';
+				}else{
+					echo '<div class="alert alert-danger alert-dismissible fade show" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><i class="fas fa-exclamation-triangle"></i><b> Please use different names for </b></div>';
+				}
+			}else{
+				echo '<div class="alert alert-danger alert-dismissible fade show" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><i class="fas fa-exclamation-triangle"></i><b> Database connection lost.</b></div>';
+			}
+		}elseif (($partipant_type == 'ars-author')||($partipant_type == '')) {
+			$payment_receipt = "N/A";
+			$studentship = "N/A";
+			
+			$sql = "INSERT INTO deligates(name,id_or_title,badge_name,affiliation,country,email,phone,rec_name,rec_address,part_type,bank_rec,std_ship,lunch,dinner,tour,bank_rec_appr,stdship_appr) VALUES('$full_name','$paper_id','$name_on_badge','$affiliation','$country','$email','$phone','$receipt_name','$receipt_address','$partipant_type','$payment_receipt','$studentship','$lunch','$dinner','$tour','$bank_appr','$std_appr')";
+
+			if ($conn->query($sql)) {
 				echo '<div class="alert alert-success alert-dismissible fade show" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><i class="fas fa-check-circle fa-lg"></i><b> Registered successfully.</b></div>';
 			}else{
-				echo '<div class="alert alert-danger alert-dismissible fade show" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><i class="fas fa-check-circle fa-lg"></i><b> Please use different names for </b></div>';
+				echo '<div class="alert alert-danger alert-dismissible fade show" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><i class="fas fa-exclamation-triangle"></i><b> Database connection lost.</b></div>';
 			}
-		}else{
-			echo '<div class="alert alert-danger alert-dismissible fade show" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><i class="fas fa-check-circle fa-lg"></i><b> Database connection lost.</b></div>';
 		}
-	}elseif ($partipant_type == 'student') {
-		
-		$sql = "INSERT INTO deligates(name,id_or_title,badge_name,affiliation,country,email,phone,rec_name,rec_address,part_type,bank_rec,std_ship,lunch,dinner,tour,bank_rec_appr,stdship_appr) VALUES('$full_name','$paper_id','$name_on_badge','$affiliation','$country','$email','$phone','$receipt_name','$receipt_address','$partipant_type','$payment_receipt','$studentship','$lunch','$dinner','$tour','$bank_appr','$std_appr')";
-
-		if ($conn->query($sql)) {
-
-			if ((!is_dir('../images/uploads/'.$name_on_badge))&&(!file_exists('../images/uploads/'.$name_on_badge))) {
-				mkdir('../images/uploads/'.$name_on_badge,0777);
-
-				move_uploaded_file($_FILES['payment_receipt']['tmp_name'], '../images/uploads/'.$name_on_badge.'/'.$payment_receipt);
-				move_uploaded_file($_FILES['studentship']['tmp_name'], '../images/uploads/'.$name_on_badge.'/'.$studentship);
-
-				echo '<div class="alert alert-success alert-dismissible fade show" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><i class="fas fa-check-circle fa-lg"></i><b> Registered successfully.</b></div>';
-			}else{
-				echo '<div class="alert alert-danger alert-dismissible fade show" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><i class="fas fa-check-circle fa-lg"></i><b> Please use different names for </b></div>';
-			}
-		}else{
-			echo '<div class="alert alert-danger alert-dismissible fade show" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><i class="fas fa-check-circle fa-lg"></i><b> Database connection lost.</b></div>';
-		}
-	}elseif (($partipant_type == 'ars-author')||($partipant_type == '')) {
-		$payment_receipt = "N/A";
-		$studentship = "N/A";
-		
-		$sql = "INSERT INTO deligates(name,id_or_title,badge_name,affiliation,country,email,phone,rec_name,rec_address,part_type,bank_rec,std_ship,lunch,dinner,tour,bank_rec_appr,stdship_appr) VALUES('$full_name','$paper_id','$name_on_badge','$affiliation','$country','$email','$phone','$receipt_name','$receipt_address','$partipant_type','$payment_receipt','$studentship','$lunch','$dinner','$tour','$bank_appr','$std_appr')";
-
-		if ($conn->query($sql)) {
-			echo '<div class="alert alert-success alert-dismissible fade show" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><i class="fas fa-check-circle fa-lg"></i><b> Registered successfully.</b></div>';
-		}else{
-			echo '<div class="alert alert-danger alert-dismissible fade show" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><i class="fas fa-check-circle fa-lg"></i><b> Database connection lost.</b></div>';
-		}
+	}else{
+		echo '<div class="alert alert-danger alert-dismissible fade show" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><i class="fas fa-exclamation-triangle"></i><b> Invalid phone number pattern.</b></div>';
 	}
 }else{
-	echo '<div class="alert alert-danger alert-dismissible fade show" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><i class="fas fa-exclamation-circle fa-lg"></i><b> Please fill required fields.</b></div>';
+	echo '<div class="alert alert-danger alert-dismissible fade show" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><i class="fas fa-exclamation-triangle"></i><b> Please fill required fields.</b></div>';
 }
 
 ?>
